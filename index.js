ce0b0e6abbafd774830fb5b944f14eb30f43d50d
LeeviKe <151837175+LeeviKe@users.noreply.github.com>
2025-06-26 20:59:57 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-app.use(express.json());
 app.use(express.static('dist'));
+app.use(express.json());
 
+const requestLogger = (req, res, next) => {
+  console.log(req.method);
+  next();
+};
+app.use(requestLogger);
 
 const morgan = require('morgan');
 morgan.token('body', (req) => JSON.stringify(req.body));
@@ -11,61 +17,124 @@ app.use(morgan(':method :url :status :response-time ms :body'));
 const cors = require('cors');
 app.use(cors());
 
-let persons = [
-  { id: 1, name: 'Arto Hellas', number: '040-123456' },
-  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' },
-];
+const Person = require('./models/person.js');
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons);
+// Checks if the given phone number is in the right format.
+function isValidPhoneNumber(number) {
+  const pattern = /^(?:\d{2}|\d{3})-\d{5,}$/;
+  return pattern.test(number);
+}
+
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      console.log(persons);
+      res.json(persons);
+    })
+    .catch(next);
 });
 
-app.get('/info', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.send(`Phonebook has info for ${persons.length} people\n${new Date()}`);
+app.get('/info', (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.set('Content-Type', 'text/plain');
+      res.send(
+        `Phonebook has info for ${persons.length} people\n${new Date()}`
+      );
+    })
+    .catch(next);
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id); // Converts the gotten id (string) into a number, matching the persons array.
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(next);
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id;
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(next);
 });
 
-app.post('/api/persons', (req, res) => {
-  const person = {
+app.post('/api/persons', (req, res, next) => {
+  const person = new Person({
     id: Math.floor(Math.random() * 1000000000),
+    name: req.body.name,
+    number: req.body.number,
+  });
+
+  if (!req.body.name || !req.body.number) {
+    return res.status(400).json({ error: 'Name or number missing' });
+  } else if (req.body.name.length < 3) {
+    return res
+      .status(400)
+      .json({ error: 'Name must be atleast 3 characters long' });
+  } else if (!isValidPhoneNumber(req.body.number)) {
+    return res.status(400).json({ error: 'Number is in the wrong format' });
+  }
+
+  return Person.findOne({ name: req.body.name }).then((name) => {
+    if (name) {
+      return res.status(400).json({ error: 'Name must be unique' });
+    } else {
+      person
+        .save()
+        .then((savedPerson) => {
+          res.status(201).json(savedPerson);
+          console.log(`Added ${person.name} ${person.number} to the phonebook`);
+        })
+        .catch(next);
+    }
+  });
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+  if (!isValidPhoneNumber(req.body.number)) {
+    return res.status(400).json({ error: 'Number is in the wrong format' });
+  }
+  const person = {
     name: req.body.name,
     number: req.body.number,
   };
 
-  if (!req.body.name || !req.body.number) {
-    return res.status(400).json({ error: 'Name or number missing' });
-  }
-
-  if (
-    persons.find((p) => p.name.toLowerCase() === req.body.name.toLowerCase())
-  ) {
-    return res.status(400).json({ error: 'Name must be unique' });
-  }
-
-  persons.push(person);
-  res.json(person);
+  return Person.findByIdAndUpdate(req.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((updated) => res.json(updated))
+    .catch(next);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'Id does not exist' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+app.use(unknownEndpoint);
+app.use(errorHandler);
